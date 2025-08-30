ba922e0c4e8fbd607436a1d74c690e2b928008c4
Surabhi Pandey <surabhi.pandey@op.iitg.ac.in>
2025-08-30 14:37:50 +0530

    More pages added

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
-
 @app.route("/meditation")
 def meditation():
     return render_template("meditation.html")
 @app.route("/mood")
 def mood_log():
     return render_template("mood.html")
+@app.route("/journal")
+def journal():
+    return render_template("journal.html")
+@app.route("/journal_copy")
+def journal_copy():
+    return render_template("journal_copy.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
